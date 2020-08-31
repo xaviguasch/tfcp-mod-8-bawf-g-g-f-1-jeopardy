ab63739d4c2b49b1602034ef3ce4c6a3e1c364fa
xaviguasch <xaviguasch@gmail.com>
2020-08-31 19:56:52 +0200

    added starter files

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('testing')
+
+/* 
+    Jeopardy
+    
+    Write an async function 
+        that uses fetch() to fetch 4 categories 
+        from https://jservice.io/api/categories
+        parameters: count, offset
+        
+    Display the categories
+        in a simplified 4x5 Jeopardy Board 
+        using CSS Grid
+*/
