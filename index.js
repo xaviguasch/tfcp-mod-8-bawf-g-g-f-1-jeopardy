616bdce272d19184340f698858c96355cfc1063a
xaviguasch <xaviguasch@gmail.com>
2020-08-31 21:13:48 +0200

    solved first part

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log('testing')
-
 /* 
     Jeopardy
     
@@ -12,3 +10,26 @@ console.log('testing')
         in a simplified 4x5 Jeopardy Board 
         using CSS Grid
 */
+
+async function getCategories() {
+  let response = await fetch('https://jservice.io/api/categories?count=4&offset=30')
+  let data = await response.json()
+  return data
+}
+
+function getCategoryHtml(category) {
+  return `
+      <div class="my-category-title">${category.title}</div>
+      <div class="my-category-clue" style="grid-row-start: 2">$100</div>
+      <div class="my-category-clue" style="grid-row-start: 3">$200</div>
+      <div class="my-category-clue" style="grid-row-start: 4">$300</div>
+      <div class="my-category-clue" style="grid-row-start: 5">$400</div>
+  `
+}
+
+getCategories().then((categories) => {
+  console.log(categories)
+  document.body.innerHTML = `<div class="board">
+      ${categories.map(getCategoryHtml).join('')}
+  </div>`
+})
