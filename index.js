61f68f1259deb8cf64e5bf460b225118af040de5
xaviguasch <xaviguasch@gmail.com>
2020-09-01 10:43:42 +0200

    solved second part

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 /* 
-    Jeopardy
+    Jeopardy (Challenge)
     
-    Write an async function 
-        that uses fetch() to fetch 4 categories 
-        from https://jservice.io/api/categories
-        parameters: count, offset
-        
-    Display the categories
-        in a simplified 4x5 Jeopardy Board 
-        using CSS Grid
+    Update getCategories to take 
+        count/offset arguments
+    Fetch 5 Categories (Update the display as necessary)
+    
+    Write a getClueHtml() function
+        calculate grid-row-start based on the input value
+        which will always be a multiple of 100 
 */
 
-async function getCategories() {
-  let response = await fetch('https://jservice.io/api/categories?count=4&offset=30')
+async function getCategories(count, offset) {
+  let response = await fetch(
+    `https://jservice.io/api/categories?count=${count}&offset=${offset}`
+  )
   let data = await response.json()
   return data
+}
+
+function getClueHtml(clueValue) {
+  return `<div class="my-category-clue" style="grid-row-start: ${
+    clueValue / 100 + 1
+  }">$${clueValue}</div>`
 }
 
 function getCategoryHtml(category) {
   return `
       <div class="my-category-title">${category.title}</div>
-      <div class="my-category-clue" style="grid-row-start: 2">$100</div>
-      <div class="my-category-clue" style="grid-row-start: 3">$200</div>
-      <div class="my-category-clue" style="grid-row-start: 4">$300</div>
-      <div class="my-category-clue" style="grid-row-start: 5">$400</div>
+      ${getClueHtml(100)}
+      ${getClueHtml(200)}
+      ${getClueHtml(300)}
+      ${getClueHtml(400)}
   `
 }
 
-getCategories().then((categories) => {
+getCategories(5).then((categories) => {
   console.log(categories)
   document.body.innerHTML = `<div class="board">
       ${categories.map(getCategoryHtml).join('')}
